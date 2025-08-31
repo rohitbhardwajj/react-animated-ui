c8333944e6d122158ed1dbe8a2e25b3a1cf3440f
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-31 18:51:52 +0530

    Done

--- src/components/animation/Transistion.jsx
+++ src/components/animation/Transistion.jsx
@@ -1,41 +1,50 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useLocation } from "react-router-dom";
+import React from "react";
 
 const Transistion = () => {
+    
+      const location = useLocation();
+    useGSAP(() => {
+  let tl = gsap.timeline();
   
-useGSAP(() => {
-      let tl = gsap.timeline();
+  tl.to(".transition-div",{
+    display: "flex"
+  })
   tl.from(".stairs", {
-    height: 0,     // bahut fast
+    height: 0,
     stagger: {
-        amount:-0.3
-    },     // chhota gap
-    ease: "power1.out" // fast ease
+      amount: -0.3,
+    },
+    ease: "power1.out",
+    duration: 0.3, // fast control ke liye
   });
+
   tl.to(".stairs", {
-    y: "100%", 
+    y: "100%",
     stagger: {
-        amount:0.3
-    },      // bahut fast
+      amount: 0.3,
+    },
+    duration: 0.3,
+    ease: "power1.in",
   });
-
-  
-});
-
+  tl.to(".transition-div",{
+    display: "none"
+  })
+   tl.to(".stairs", {
+    y: "0%",
+  });
+}, [location.pathname]);  // yaha dependency pass karo
 
   return (
-    <div className='transition-div'>
-        <div className="stairs" style={{ backgroundColor: "black" }}>
-        </div>
-        <div className="stairs" style={{ backgroundColor: "black" }}>
-        </div>
-        <div className="stairs" style={{ backgroundColor: "black" }}>
-        </div>
-        <div className="stairs" style={{ backgroundColor: "black" }}>
-        </div>
+    <div className="transition-div">
+      <div className="stairs" style={{ backgroundColor: "black" }}></div>
+      <div className="stairs" style={{ backgroundColor: "black" }}></div>
+      <div className="stairs" style={{ backgroundColor: "black" }}></div>
+      <div className="stairs" style={{ backgroundColor: "black" }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Transistion
+export default Transistion;
